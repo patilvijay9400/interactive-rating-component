9d300dd989b926fe30c3b6347657f03924693d34
patilvijay9400 <patilvijay416940@gmail.com>
2023-01-18 13:05:56 +0530

    converted class into arrow function and functional lifting used to show selected rating

--- src/Components/RatingForm.js
+++ src/Components/RatingForm.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react'
 import Rating from "react-rating"
 
-export default function RatingForm() {
-  const [rating1, setRating1] = useState(0);
+export default function RatingForm(props) {
+  
   return (
     <div>
       <Rating
         className="rating"
-        initialRating={rating1}
-        onClick={rate => setRating1(rate)}
+        initialRating={props.selectedRating}
+        onClick={rate => props.setSelectedRating(rate)}
       />
-      <p>Rating: {rating1}</p>
     </div>
   )
 }
